20af1c5a125b0acc8d92b920224d2c28b3810259
Slim Abida <slimabida21@gmail.com>
2026-01-07 16:23:12 +0100

    feat: intégration localStorage pour persistance des posts et correction du contexte

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -1,41 +1,67 @@
 import { createContext } from "react";
-import { useState } from "react";
-export const PostContext = createContext();
+import { useState, useEffect } from "react";
+export const PostContext = createContext({
+  posts: [],
+  setPosts: () => {},
+  addPost: () => {},
+  findPostById: () => {},
+  updatePost: () => {},
+});
 
 function PostProvider(props) {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Hello Wordl",
-      content: "welcome to my new blog",
-      author: "John Doe",
-      date: new Date().toLocaleDateString(),
-    },
-    {
-      id: 2,
-      title: "My First Post",
-      content:
-        "This is my first post on my blog. I'm excited to share my thoughts and ideas with you.",
-      author: "John Doe",
-      date: new Date().toLocaleDateString(),
-    },
-    {
-      id: 3,
-      title: "My Second Post",
-      content:
-        "This is my second post on my blog. I'm excited to share my thoughts and ideas with you.",
-      author: "John Doe",
-      date: new Date().toLocaleDateString(),
-    },
-    {
-      id: 4,
-      title: "My Third Post",
-      content:
-        "This is my third post on my blog. I'm excited to share my thoughts and ideas with you.",
-      author: "John Doe",
-      date: new Date().toLocaleDateString(),
-    },
-  ]);
+  const [posts, setPosts] = useState(() => {
+    const storedPosts = localStorage.getItem("posts");
+    if (storedPosts) {
+      return JSON.parse(storedPosts);
+    } else {
+      return [
+        //Post 1 //
+        {
+          id: 1,
+          title: "Hello Wordl",
+          content: "welcome to my new blog",
+          author: "John Doe",
+          date: new Date().toLocaleDateString(),
+        },
+        // Post 2 //
+        {
+          id: 2,
+          title: "My First Post",
+          content:
+            "This is my first post on my blog. I'm excited to share my thoughts and ideas with you.",
+          author: "John Doe",
+          date: new Date().toLocaleDateString(),
+        },
+        // Post 3 //
+        {
+          id: 3,
+          title: "My Second Post",
+          content:
+            "This is my second post on my blog. I'm excited to share my thoughts and ideas with you.",
+          author: "John Doe",
+          date: new Date().toLocaleDateString(),
+        },
+        // Post 4 //
+        {
+          id: 4,
+          title: "My Third Post",
+          content:
+            "This is my third post on my blog. I'm excited to share my thoughts and ideas with you.",
+          author: "John Doe",
+          date: new Date().toLocaleDateString(),
+        },
+      ];
+    }
+  });
+
+  /**
+   * UseEffect qui sauvegarde automatiquement
+   * dans localStorage à chaque modification de posts
+   *  */
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
 
   // Add a new Post //
   function addPost(newPost) {
